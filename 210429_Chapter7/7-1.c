5276f705fb083a27a76118fa32f6f2990b95773c
SeoDongWoo1216 <dw6642@naver.com>
2021-04-29 17:32:17 +0900

    0429_7-1, 계산기수정

--- 210429_Chapter7/7-1.c
+++ 210429_Chapter7/7-1.c
@@ -1,1 +1,21 @@
+// 2개의 함수로 만든 프로그램
 #include<stdio.h>
+
+int sum(int x, int y);    // sum 함수 선언
+
+int main(void) {
+	int a = 10, b = 20;
+	int result;
+
+	result = sum(a, b);    // 함수 호출
+	printf("result : %d\n", result);
+
+	return 0;
+}
+
+int sum(int x, int y)  // 함수 정의 시작
+{
+	int temp;
+	temp = x + y;
+	return temp;       // temp 값 반환
+}
--- 210429_Chapter7/Calculator_v4.c
+++ 210429_Chapter7/Calculator_v4.c
@@ -1,29 +1,25 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-#include<ctype.h>
 
 int Sum(int x, int y)
 {
 	return x + y;
 }
-
 int Sub(int x, int y)
 {
 	return x - y;
 }
-
 int Mul(int x, int y)
 {
 	return x * y;
 }
-
 double Div(int x, int y)
 {
 	return (double)x / (double)y;
 }
 
 
-int main() 
+int main()
 {
 	int firNum = 0;
 	int secNum = 0;
@@ -31,84 +27,82 @@ int main()
 	int result = 0;
 	double divResult = 0;
 
-	while (1) 
+	while (1)
 	{
 		printf("----------------------------------\n");
 		printf("Calculator_v3\n");
 		printf("Created : 2021-04-29\n");
 		printf("Author : 서동우\n ");
 		printf("----------------------------------\n");
-		printf("계산할 두 정수(0~100까지)를 입력해주세요 : ");
+		printf("계산할 두 정수를 입력해주세요 : ");
 		scanf("%d %d", &firNum, &secNum);
 
-		if ( !(0 >= firNum && firNum >= 100) ) 
+		// TODO:두 정수말고 다른거 입력했을때 오류처리
+
+
+		printf("연산 기호를 입력해주세요(+,-,*,/) : ");
+		scanf(" %c", &cal);
+
+		switch (cal)
 		{
-			printf("범위에 맞는 숫자를 입력해주세요\n");
-			continue;
-		}
-		else if (!(0 >= secNum && secNum >= 100)) 
-		{
-			printf("범위에 맞는 숫자를 입력해주세요\n");
+		case '+':
+			result = Sum(firNum, secNum);
+			break;
+
+		case '-':
+			result = Sub(firNum, secNum);
+			break;
+
+		case '*':
+			result = Mul(firNum, secNum);
+			break;
+
+		case '/':
+			if (secNum == 0)
+			{
+				printf("0으로는 나눌 수 없습니다\n\n");
+				continue;
+			}
+			else
+			{
+				divResult = Div(firNum, secNum);
+				printf("%d %c %d = %.2lf\n", firNum, cal, secNum, divResult);
+				continue;
+			}
+
+			/*case 'q':
+				printf("프로그램을 종료합니다.");
+				exit(0);*/
+
+		default:
+			printf("연산 기호를 입력해주세요\n");
 			continue;
 		}
 
+		printf("%d %c %d = %d", firNum, cal, secNum, result);
+		printf("\n");
+
+		int RepeatNum = 0;
+		printf("반복하시겠습니까?(11, 00) : ");
+		scanf("%d", &RepeatNum);
+		printf("\n");
+
+		if (RepeatNum == 11) 
+		{
+			continue;
+		}
+		if (RepeatNum == 00)
+		{
+			printf("종료합니다\n");
+			exit(1);
+		}
 		else 
 		{
-			printf("연산 기호를 입력해주세요(+,-,*,/) : ");
-			scanf(" %c", &cal);
-
-			switch (cal)
-			{
-			case '+':
-				result = Sum(firNum, secNum);
-				break;
-
-			case '-':
-				result = Sub(firNum, secNum);
-				break;
-
-			case '*':
-				result = Mul(firNum, secNum);
-				break;
-
-			case '/':
-				if (secNum == 0)
-				{
-					printf("0으로는 나눌 수 없습니다\n");
-				}
-				else
-				{
-					divResult = Div(firNum, secNum);
-					printf("%d %c %d = %.2lf\n", firNum, cal, secNum, divResult);
-					continue;
-				}
-				break;
-
-				/*case 'q':
-					printf("프로그램을 종료합니다.");
-					exit(0);*/
-
-			default:
-				printf("연산 기호를 입력해주세요");
-				break;
-			}
-
-			printf("%d %c %d = %d", firNum, cal, secNum, result);
-			printf("\n");
-
-
-			int RepeatNum = 0;
-			printf("반복하시겠습니까?(1, 0) : ");
-			scanf("%d", &RepeatNum);
-			printf("\n");
-
-			if (RepeatNum == 0)
-			{
-				printf("종료합니다\n");
-				exit(1);
-			}
+			printf("똑바로 입력해주세요");
+			
 		}
-		
+
+
 	}
 }
 
