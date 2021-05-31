977c84660bb54c0dbeaf4af315ea83ae5357f92e
SeoDongWoo1216 <dw6642@naver.com>
2021-05-31 17:14:51 +0900

    21-05-31 아무입력안했을때 오류 수정

--- IoT_Test/휘트니스 회원관리 프로그램.c
+++ IoT_Test/휘트니스 회원관리 프로그램.c
@@ -1,94 +1,101 @@
 /*
+휘트니스 회원관리 프로그램
 신규회원 등록은 회원번호와 이름, 체중을 입력하고, heap 영역에 저장
 
 회원번호로 음수를 입력하면 입력이 종료되며, 총 회원 수, 평균 체중, 최고 체중을 갖는 회원 정보가 출력된다.
 
 scanf로 회원번호, 이름, 체중입력 한것을 heap 영역에 저장
-음수를 입력하면 총 회원수, 평균 체중, 최고 체중을 갖는 회원의 정보 출력
+회원번호에 음수를 입력하면 총 회원수, 평균 체중, 최고 체중을 갖는 회원의 정보 출력
+
+1. 구조체선언
+2. main에서 회원정보를 입력받고 입력받은 만큼의 메모리에 동적할당
+3. 회원번호에서 -1을 입력했을때 회원정보가 뜨도록 구현
 */
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct 
+typedef struct      // 구조체 선언
 {
 	int num;
 	char name[20];
 	double weight;
 } Fitness;
 
+
+// 함수부
 void total_number(int count);                     // 전체 등록 회원 수 반환
 double average_weight(Fitness** pary, int count); // 평균 체중 반환
 int max_weight(Fitness** pary, int count);        // 최대 체중 회원의 index 반환
 void print_info(Fitness** pary, int index);       // 회원 정보 출력
 void free_ary(Fitness** pary, int count);         // 동적 할당 영역 해제
 
+
 int main()
 {
 	int count = 0;        // 지금 몇번째 회원인지 index 저장 변수
-
 	Fitness* ary[100];    // 포인터 배열 선언
 
-
-
-	// 임시로 정보들을 입력받고, 그만큼 동적할당한다음에 구조체배열에 대입하는 걸로 다시짜보기
-
-
-	for (int i = 0; i < sizeof(ary) / sizeof(Fitness*); i++)
+	for (int i = 0; i < sizeof(ary) / sizeof(Fitness*); i++) 
 	{
-		ary[i] = (Fitness*)malloc(sizeof(Fitness));    // 정보를 입력할때 동적할당
+		int tempNum = 0;                                    // 입력받을 임시 변수 선언
+		char tempName[20] = { 0 };
+		double tempWeight = 0.0;
 
-		if (ary == NULL)
+		printf("회원 번호 입력(음수를 입력하면 종료) : ");
+		scanf("%d", &tempNum);
+		if (tempNum < 0)
 		{
-			printf("메모리 할당 실패\n");
+			printf("음수를 입력하셨으니 종료합니다.\n");
 			break;
 		}
 
-		printf("회원번호 입력(음수입력하면 종료) : ");
-		scanf("%d", &ary[i]->num);
-		if (ary[i]->num < 0)               // 음수 입력했을때 종료
+		printf("회원 이름 입력 : ");
+		scanf(" %s", tempName);
+
+		printf("회원 체중 입력 : ");
+		scanf("%lf", &tempWeight);
+
+		ary[i] = (Fitness*)malloc(sizeof(Fitness));        // 입력받은 정보를 메모리에 동적할당
+		if (ary[i] != NULL)
 		{
-			break;
+			ary[i]->num = tempNum;
+			strcpy(ary[i]->name, tempName);
+			ary[i]->weight = tempWeight;
 		}
-		
-		printf("이름입력 : ");
-		char* tempName[20];
-		scanf(" %s", ary[i]->name);        // 이름은 안들어가서 임시로 만든다음에 strcpy로 넣었음
-		strcpy(tempName, ary[i]->name);
 
-		printf("체중입력 : ");
-		scanf("%lf", &ary[i]->weight);
-
-		count++;                          // 입력이 끝났으니 index++ 해줌
-
-		int Restart = 0;                 
-		printf("또 입력하시겠습니까?(자연수 : 예, 음수 : 아니오) : ");   // 또 입력할지 물어봄
-		scanf("%d", &Restart);
-		if (Restart < 0)
-		{
-			break;
-		}
-		printf("\n\n");
+		printf("\n");
+		count++;
 	}
 
-	
+
+
 	// 전체 몇명 등록됬는지 출력
 	total_number(count);
 
 
 	// 회원들의 평균 체중 반환
 	double averagetemp = average_weight(ary, count);
-	printf("\n\n평균 체중 : %.2lf kg\n", averagetemp / count); // 모든 회원들의 체중을 반환받고 count를 나눠줘서 출력
+	if (averagetemp != 0)     // averagetemp = 0 은 count가 0일때 반환하므로 회원정보가 없을때이다.
+	{
+		printf("\n-----------------평균체중 출력-----------------\n");
+		printf("평균 체중 : %.2lf kg\n", averagetemp); // 모든 회원들의 평균체중을 반환받고 출력
+		printf("-----------------------------------------------\n");
+	}
 
 
 	// 제일 무거운분의 정보 출력
 	int max = max_weight(ary, count);
-	printf("\n-------최대 체중을 가진 회원정보------\n");
-	printf("번호 : %d\n", ary[max]->num);
-	printf("이름 : %s\n", ary[max]->name);
-	printf("체중 : %.2lf\n", ary[max]->weight);
-	printf("--------------------------------------\n");
+	
+	if (max != 0)            // max = 0은 count가 0이므로 회원정보가 없을때이다.
+	{
+		printf("\n-------최대 체중을 가진 회원정보------\n");
+		printf("번호 : %d\n", ary[max]->num);
+		printf("이름 : %s\n", ary[max]->name);
+		printf("체중 : %.2lf\n", ary[max]->weight);
+		printf("--------------------------------------\n");
+	}
 
 
 	// 전체 회원들의 정보 출력
@@ -97,7 +104,6 @@ int main()
 
 	// 동적할당 해제
 	free_ary(ary, count);
-	//printf("여기까지 오류없으면 정상적으로 출력된듯\n");
 	
 	return 0;
 }
@@ -108,27 +114,29 @@ int main()
 /// </summary>
 void total_number(int count)
 {
-	printf("\n\n\n전체 회원 수는 %d명입니다.\n", count);  // 총 회원수는 count에 들어가있으니 count를 출력
+	printf("\n-----------------전체 회원 수-------------------\n");
+	printf("전체 회원 수는 %d명입니다.\n", count);  // 총 회원수는 count에 들어가있으니 count를 출력
+	printf("------------------------------------------------\n");
 }
 
 
 /// <summary>
-/// 회원들의 평균 체중 반환 함수(정확히는 모든 회원의 체중을 합해서 반환)
+/// 회원들의 평균 체중 반환 함수
 /// </summary>
 double average_weight(Fitness** pary, int count)
 {
+	if (count == 0)
+	{
+		printf("(평균체중반환) 회원이 없습니다\n");
+		return 0;
+	}
 	double avgtemp = 0;                  
 	for (int i = 0; i < count; i++)
 	{
-		if (pary == NULL)
-		{
-			printf("저장된 메모리가 없습니다\n");
-			break;
-		}
-		avgtemp += pary[i]->weight;       // 
+		
+		avgtemp += pary[i]->weight;           // 회원들의 체중을 다 더해줌
 	}
-	//printf("(함수안에서)평균 체중 : %.2lf kg\n", avgtemp / count);
-	return avgtemp;
+	return (avgtemp/count);
 }
 
 
@@ -137,6 +145,11 @@ double average_weight(Fitness** pary, int count)
 /// </summary>
 int max_weight(Fitness** pary, int count)
 {
+	if (count == 0)
+	{
+		printf("(최대체중) 회원이 없습니다.");
+		return 0;
+	}
 	int temp = 0;
 	int max = pary[0]->weight;
 	for (int i = 1; i < count; i++)
@@ -147,9 +160,6 @@ int max_weight(Fitness** pary, int count)
 			temp = i;
 		}
 	}
-	//printf("이름 : %s\n", pary[temp]->name);
-	//printf("번호 : %d\n", pary[temp]->num);
-	//printf("체중 : %.2lf\n\n", pary[temp]->weight);
 
 	return temp;    // 제일 무거운분의 인덱스를 반환
 }
@@ -165,6 +175,11 @@ void print_info(Fitness** pary, int index)
 
 	printf("\n");
 
+	if (index == 0)
+	{
+		printf("(회원정보) 회원이 없습니다.\n");
+		return;
+	}
 	// 전체 등록된 회원 출력
 	for (int i = 0; i < index; i++)
 	{
@@ -182,6 +197,11 @@ void print_info(Fitness** pary, int index)
 /// </summary>
 void free_ary(Fitness** pary, int count)
 {
+	if (count == 0) 
+	{
+		printf("(free영역) 해제할 메모리가 없습니다.\n");
+		return;
+	}
 	printf("\n");
 	for (int i = 0; i < count; i++)
 	{
